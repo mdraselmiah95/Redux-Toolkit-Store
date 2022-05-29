e242b0d08ead48c0e4998dda5d4971eb768a5545
mdraselmiah95 <russelroy15@gmail.com>
2022-05-30 00:12:54 +0600

    WIP on cartStore and add to cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Cart = () => {
+  const products = useSelector((state) => state.cart);
   return (
     <div>
-      <h2>This is cart</h2>
+      <h3>Cart</h3>
+      <div className="cartWrapper">
+        {products.map((product) => (
+          <div key={product.id} className="cartCard">
+            <img src={product.image} alt="" />
+            <h5>{product.title}</h5>
+            <h5>{product.price}</h5>
+            <button className="btn">Remove</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
