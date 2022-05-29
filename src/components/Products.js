0405fd654e80435f89040a83734185af1b9dc23e
mdraselmiah95 <russelroy15@gmail.com>
2022-05-29 23:54:58 +0600

    WIP on cartStore and add to cart

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { add } from "../store/cartSlice";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     // const fetchProducts = async () => {
@@ -15,6 +18,10 @@ const Products = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  const handleAdd = (product) => {
+    dispatch(add(product));
+  };
+
   return (
     <div className="productsWrapper">
       {products.map((product) => (
@@ -22,7 +29,9 @@ const Products = () => {
           <img src={product.image} alt="" />
           <h4>{product.title}</h4>
           <h5>${product.price}</h5>
-          <button className="btn">Add to cart</button>
+          <button className="btn" onClick={() => handleAdd(product)}>
+            Add to cart
+          </button>
         </div>
       ))}
     </div>
