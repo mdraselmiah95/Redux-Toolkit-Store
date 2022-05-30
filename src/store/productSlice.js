759ea5bf9e1543e0f5ae776f778f2a0c049f78c7
mdraselmiah95 <russelroy15@gmail.com>
2022-05-30 22:54:01 +0600

    WIP on product Slice

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -14,14 +14,20 @@ const productSlice = createSlice({
   },
   reducers: {
     setProducts(state, action) {
-      //   const res = await fetch("https://fakestoreapi.com/products"); don't do that
-      // setProducts(state, action) {
-      //     state.data = action.payload;
-      // },
-      // setStatus(state, action) {
-      //     state.status = action.payload;
-      // },
+      state.data = action.payload;
     },
+    setStatus(state, action) {
+      state.state = action.payload;
+    },
+    // setProducts(state, action) {
+    //     const res = await fetch("https://fakestoreapi.com/products"); don't do that
+    //   setProducts(state, action) {
+    //       state.data = action.payload;
+    //   },
+    //   setStatus(state, action) {
+    //       state.status = action.payload;
+    //   },
+    // },
     extraReducers: (builder) => {
       builder.addCase();
     },
@@ -34,10 +40,15 @@ export default productSlice.reducer;
 //Thunk
 export function fetchProducts() {
   return async function fetchProductsThunk(dispatch, getState) {
+    dispatch(setStatus(STATUSES.LOADING));
     try {
       const res = await fetch("https://fakestoreapi.com/products");
       const data = await res.json();
       dispatch(setProducts(data));
-    } catch (error) {}
+      dispatch(setStatus(STATUSES.IDLE));
+    } catch (err) {
+      console.log(err);
+      dispatch(setStatus(STATUSES.ERROR));
+    }
   };
 }
