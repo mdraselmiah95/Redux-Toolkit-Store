501714d0d5bc26edcfbc700dbc2637ddd41225b0
mdraselmiah95 <russelroy15@gmail.com>
2022-05-30 23:41:19 +0600

    WIP on product Slice

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,5 +1,30 @@
 const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
 
+//Thunk
+
+// export function fetchProducts() {
+//   return async function fetchProductsThunk(dispatch, getState) {
+//     dispatch(setStatus(STATUSES.LOADING));
+//     try {
+//       const res = await fetch("https://fakestoreapi.com/products");
+//       const data = await res.json();
+//       dispatch(setProducts(data));
+//       dispatch(setStatus(STATUSES.IDLE));
+//     } catch (err) {
+//       console.log(err);
+//       dispatch(setStatus(STATUSES.ERROR));
+//     }
+//   };
+// }
+
+//createAsyncThunk
+
+const fetchProducts = createAsyncThunk("products/fetch", async () => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+  return data;
+});
+
 export const STATUSES = Object.freeze({
   IDLE: "idle",
   ERROR: "error",
@@ -13,42 +38,27 @@ const productSlice = createSlice({
     status: STATUSES.IDLE,
   },
   reducers: {
-    setProducts(state, action) {
-      state.data = action.payload;
-    },
-    setStatus(state, action) {
-      state.state = action.payload;
-    },
     // setProducts(state, action) {
-    //     const res = await fetch("https://fakestoreapi.com/products"); don't do that
-    //   setProducts(state, action) {
-    //       state.data = action.payload;
-    //   },
-    //   setStatus(state, action) {
-    //       state.status = action.payload;
-    //   },
+    //   state.data = action.payload;
     // },
-    extraReducers: (builder) => {
-      builder.addCase();
-    },
+    // setStatus(state, action) {
+    //   state.state = action.payload;
+    // },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.data = STATUSES.payload;
+        state.status = STATUSES.IDLE;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = STATUSES.ERROR;
+      });
   },
 });
 
 export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
-
-//Thunk
-export function fetchProducts() {
-  return async function fetchProductsThunk(dispatch, getState) {
-    dispatch(setStatus(STATUSES.LOADING));
-    try {
-      const res = await fetch("https://fakestoreapi.com/products");
-      const data = await res.json();
-      dispatch(setProducts(data));
-      dispatch(setStatus(STATUSES.IDLE));
-    } catch (err) {
-      console.log(err);
-      dispatch(setStatus(STATUSES.ERROR));
-    }
-  };
-}
